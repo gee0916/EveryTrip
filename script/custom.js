a30220527d9fc36a46a1da8f3548efcbb9c3883f
gee0916 <hgee9816@gmail.com>
2024-08-24 18:58:47 +0900

    💄 슬라이드, 카테고리 디자인 ✨ 슬라이드 기능

---
+++ script/custom.js
@@ -0,0 +1,13 @@
+$(function () {
+  /* front slider */
+  $(".front-slider-items").slick({
+    infinite: true,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    fade: false,
+    speed: 300,
+    pauseOnHover: true,
+  });
+});
