b2a12bfa4e937dc2a41ae8ff18db0b4b4b95a5ea
gee0916 <hgee9816@gmail.com>
2024-08-26 22:05:21 +0900

    💄 포커스, 베스트 상품, 얼리버드 디자인 ✨ 포커스 슬라이드, 카운트다운

--- script/custom.js
+++ script/custom.js
@@ -2,12 +2,31 @@ $(function () {
   /* front slider */
   $(".front-slider-items").slick({
     infinite: true,
-    arrows: false,
+    arrows: true,
     dots: true,
     autoplay: true,
     autoplaySpeed: 2500,
     fade: false,
     speed: 300,
     pauseOnHover: true,
+  });
+
+  /* front slider */
+  $(".focus-package-items1").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $(".controls1 .prevArrow"),
+    nextArrow: $(".controls1 .nextArrow"),
+  });
+
+  $(".focus-package-items2").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $(".controls2 .prevArrow"),
+    nextArrow: $(".controls2 .nextArrow"),
+  });
+
+  $(".countdown").countdown("2030/12/31", function (event) {
+    $(this).html(event.strftime("<em>%H</em> : <em>%M</em> : <em>%S</em>"));
   });
 });
