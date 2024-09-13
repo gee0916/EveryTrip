db588b434b9315014c9683d09e081f906f174fab
gee0916 <hgee9816@gmail.com>
2024-09-13 21:40:43 +0900

    💄 회원가입, 로그인 구현 ✨ 버튼클릭이벤트

--- script/include.js
+++ script/include.js
@@ -8,6 +8,27 @@ $(function () {
     $("section").click(function () {
       $(".mega-navi").stop().slideUp();
     });
+
+    /* member-register */
+    $(".toggle-pw").click(function () {
+      // toggle icon shape
+      $(this).toggleClass("bi-eye");
+      // toggle input type
+      let inputType = $(this).parent().children("input").attr("type");
+      if (inputType == "password") {
+        $(this).parent().children("input").attr("type", "text");
+      } else {
+        $(this).parent().children("input").attr("type", "password");
+      }
+    });
+
+    /* header loogin modal */
+    $(".btn-login, .login-member").click(function () {
+      $(".member-login-overlay").fadeIn();
+    });
+    $(".btn-modal-close").click(function () {
+      $(".member-login-overlay").fadeOut();
+    });
   });
   $(".footer-include").load("/include/footer.html");
 
