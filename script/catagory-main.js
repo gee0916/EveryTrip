77a19790bace337004b50ef21f6d04d0d76ac330
gee0916 <hgee9816@gmail.com>
2024-09-19 13:57:47 +0900

    💄 예약내역, 카테고리메인 모바일 ✨ 카테고리메인 기능 추가

--- script/catagory-main.js
+++ script/catagory-main.js
@@ -19,5 +19,23 @@ $(function () {
   $(".focus-package-items").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 });
